80cab1fb8bf957717dc585601771b6f5a389e8ee
Alejandro <alecarvalho950@gmail.com>
2023-10-12 19:12:00 -0300

    Alterando e adicionando novas funções de mousedown e mouseup nos botões

--- script.js
+++ script.js
@@ -13,17 +13,25 @@ formulario.addEventListener('submit', adicionaTarefa)
 buttonLimpar.addEventListener('click', limpaTarefas)
 window.addEventListener('load', focar)
 botaoApagaTudo.addEventListener('click', apagaTudo)
+
+inputButton.addEventListener('mousedown', ClicaMudaCorAdicionaTarefa)
+inputButton.addEventListener('mouseup', clicaRestauraCorAdicionaTarefa)
 inputButton.addEventListener('mouseenter', mudaCorAdiciona)
 inputButton.addEventListener('mouseout', restauraCorAdiciona)
+
+buttonLimpar.addEventListener('mousedown', clicaMudaCorLimpar)
+buttonLimpar.addEventListener('mouseup', clicaRestauraCorLimpar)
 buttonLimpar.addEventListener('mouseenter', mudaCorLimpar)
 buttonLimpar.addEventListener('mouseout', restauraCorLimpar)
+
+botaoApagaTudo.addEventListener('mousedown', clicaMudaCorApagaTudo)
+botaoApagaTudo.addEventListener('mouseup', clicaRestauraCorApagaTudo)
 botaoApagaTudo.addEventListener('mouseenter', mudaCorApagaTudo)
 botaoApagaTudo.addEventListener('mouseout', restauraCorApagaTudo)
+
 inputText.addEventListener('mouseenter', fazZoom)
 inputText.addEventListener('mouseout', desfazZoom)
 inputText.addEventListener('input', aoDigitar)
-
-
 
 //Função de focar no campo de digitar a tarefa a ser adicionada
 function focar(event) {
@@ -37,12 +45,15 @@ event.preventDefault();
 const itemLista = window.document.createElement('li');
 itemLista.textContent = inputText.value
 listaTarefa.appendChild(itemLista);
+itemLista.style.width = '350px';
+
+inputText.value = '';
+inputText.focus();
 
 const botaoExcluir = window.document.createElement('button');
 botaoExcluir.textContent = 'Excluir';
-botaoExcluir.id = 'botaoExcluiTarefa';
 window.document.body.appendChild(botaoExcluir);
-const botaoExcluiTarefa = window.document.getElementById('botaoExcluiTarefa')
+
 botaoExcluir.addEventListener('mouseenter', mudaCorExcluiTarefa)
 botaoExcluir.addEventListener('mouseout', restauraCorExcluiTarefa)
 itemLista.appendChild(botaoExcluir);
@@ -50,12 +61,20 @@ botaoExcluir.addEventListener('click', excluiTarefa)
 
 const botaoConcluido = window.document.createElement('button')
 botaoConcluido.textContent = 'Concluir';
-botaoConcluido.id = 'botaoConcluirTarefa';
 itemLista.appendChild(botaoConcluido);
+botaoConcluido.addEventListener('mouseenter', mudaCorConcluiTarefa)
+botaoConcluido.addEventListener('mouseout', restauraCorConcluiTarefa)
 botaoConcluido.addEventListener('click', concluiTarefa)
+}
 
-inputText.value = '';
-inputText.focus();
+function ClicaMudaCorAdicionaTarefa(event) {
+    inputButton.style.transition = 'background 0.1s';
+    inputButton.style.background = 'black';
+}
+
+function clicaRestauraCorAdicionaTarefa(event) {
+    inputButton.style.transition = 'background 0.1s';
+    inputButton.style.background = '#05ad02';
 }
 
 
@@ -76,10 +95,25 @@ function excluiTarefa(event) {
 function concluiTarefa(event) {
     const itemLista = event.target.parentNode;
     const itemListaConcluida = window.document.createElement('li');
-    itemListaConcluida.textContent = itemLista.textContent.replace('Remover Tarefa', '').replace('Concluir', '')
+    itemListaConcluida.textContent = itemLista.textContent.replace('Excluir', '').replace('Concluir', '')
     tarefaConcluida.appendChild(itemListaConcluida);
+    itemListaConcluida.style.background = '#CEECF5';
+    itemListaConcluida.style.color = 'blue';
+    itemListaConcluida.style.font = 'bold 18pt Arial';
+    itemListaConcluida.style.border = 'solid 5px #00FF40';
+
 
     listaTarefa.removeChild(itemLista);
+}
+
+function clicaMudaCorApagaTudo(event) {
+    botaoApagaTudo.style.transition = 'background 0.1s';
+    botaoApagaTudo.style.background = 'black';
+}
+
+function clicaRestauraCorApagaTudo(event) {
+    botaoApagaTudo.style.transition = 'background 0.1s';
+    botaoApagaTudo.style.background = '#ff7300';
 }
 
 function apagaTudo(event) {
@@ -105,8 +139,18 @@ function restauraCorAdiciona(event) {
     inputButton.style.transform = 'scale(1.0)';
 }
 
+function clicaMudaCorLimpar(event) {
+    buttonLimpar.style.transition = 'background 0.1s';
+    buttonLimpar.style.background = 'black';
+}
+
+function clicaRestauraCorLimpar(event) {
+    buttonLimpar.style.transition = 'background 0.1s';
+    buttonLimpar.style.background = '#ff7300';
+}
+
 function mudaCorLimpar(event) {
-    buttonLimpar.style.background = 'red';
+    buttonLimpar.style.background = '#FF0000';
     buttonLimpar.style.transition = 'transform 0.2s';
     buttonLimpar.style.transform = 'scale(1.2)';
 }
@@ -118,7 +162,7 @@ function restauraCorLimpar(event) {
 }
 
 function mudaCorApagaTudo(event) {
-    botaoApagaTudo.style.background = 'red';
+    botaoApagaTudo.style.background = '#FF0000';
     botaoApagaTudo.style.transition = 'transform 0.2s';
     botaoApagaTudo.style.transform = 'scale(1.2)';
 }
@@ -132,15 +176,15 @@ function restauraCorApagaTudo(event) {
 function fazZoom(event){
     inputButton.style.transition = 'transform 0.2s';
     inputText.style.transform ='scale(1.1)';
-    inputText.style.border = 'solid 5px blue';
-    inputText.style.font = 'normal 18pt Arial';
+    inputText.style.border = 'solid 5px yellow';
+    inputText.style.font = 'normal 16pt Arial';
 }
 
 function desfazZoom(event){
     inputText.style.transition = 'transform 0.2s';
     inputText.style.transform = 'scale(1.0)';
-    inputText.style.border = 'solid 5px black';
-    inputText.style.font = 'normal 10pt Arial';5
+    inputText.style.border = 'solid 5px #FFF';
+    inputText.style.font = 'normal 10pt Arial';
 }
 
 function aoDigitar(event) {
@@ -151,13 +195,29 @@ function aoDigitar(event) {
 }
 
 function mudaCorExcluiTarefa(event) {
+    const botaoExcluiTarefa = event.target;
     botaoExcluiTarefa.style.background = 'red';
     botaoExcluiTarefa.style.transition = 'transform 0.2s';
     botaoExcluiTarefa.style.transform = 'scale(1.1)';
 }
 
 function restauraCorExcluiTarefa(event) {
+    const botaoExcluiTarefa = event.target;
     botaoExcluiTarefa.style.background = '#ff7300';
     botaoExcluiTarefa.style.transition = 'transform 0.2s';
     botaoExcluiTarefa.style.transform = 'scale(1.0)';
+}
+
+function mudaCorConcluiTarefa(event) {
+    const botaoConcluiTarefa = event.target;
+    botaoConcluiTarefa.style.background = 'red';
+    botaoConcluiTarefa.style.transition = 'transform 0.2s';
+    botaoConcluiTarefa.style.transform = 'scale(1.1)';
+}
+
+function restauraCorConcluiTarefa(event) {
+    const botaoConcluiTarefa = event.target;
+    botaoConcluiTarefa.style.background = '#ff7300';
+    botaoConcluiTarefa.style.transition = 'transform 0.2s';
+    botaoConcluiTarefa.style.transform = 'scale(1.0)';
 }
